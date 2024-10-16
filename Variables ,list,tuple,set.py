6d15804a593c7338a33a8c77ddfff1b42bc0487d
priyanka-0605 <149793516+priyanka-0605@users.noreply.github.com>
2024-10-16 15:05:05 +0530

    Add files via upload
    python

---
+++ Variables ,list,tuple,set.py
@@ -0,0 +1,85 @@
+# variables:
+# x=2
+# x assign 2
+# variables is a box where we can change the value
+# print(x+3)
+# y=10
+# x=9
+# print(x+y)
+# name="youtube"
+# print( name+" rock")
+# string is a combination of characters
+# print(name[0])
+# print(name[3])
+# 0{index} is starting value and 2 i ending value
+# print(name[0:2])
+# string in python is immutable
+# 0{index} is starting value and 2 is ending value
+# x=9
+# print(x+y)
+# list:
+# list is the collecion of items,mutable(means we can change the elements),
+num=[2,3,4,5,6]
+print(num)
+print(num[0])
+print(num[2:4])
+print(num[-1])
+# -1 is for last element 
+name=["priya","nikita","kajal","preeti"]
+print(name[0])
+values=[9.5,"himani",23]
+print(values)
+mil=[num,values]
+print(mil)
+# num.append(457)
+# element will join at the end of the list
+# print(num)
+num.insert(2,78)
+# 2 means that the element will join at the 2nd position
+# print(num)
+# num.pop(1)
+# pop function used to delete element of 1st index
+# print(num)
+# del num[2:]
+# despite 2 elements everything will be deleted
+# print(num)
+# num.extend([28,34,5,6,67])
+# extend fun used to add elements
+# print(num)
+print(min(num))
+print(max(num))
+print(sum(num))
+num.sort()
+# sort used to sorting the list
+print(num)
+# tuple
+# tuple is the collection of items but immutable (means we can't change)
+tup=(12,3,45,6)
+print(tup[1])
+# set
+s={22,34,5,66,7}
+print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
